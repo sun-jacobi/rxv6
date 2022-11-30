35139874009002a511a421ee4788a62892da02cb
sun-jacobi <sun1011jacobi@gmail.com>
2022-11-30 15:03:24 +0900

    done start

--- src/risc.rs
+++ src/risc.rs
@@ -1,1 +1,49 @@
+pub(crate) mod tp {
+    pub(crate) fn write(bits: usize) {
+        use core::arch::asm;
+        unsafe {
+            asm!("mv tp, {}", in(reg) bits);
+        }
+    }
+}
 
+pub(crate) fn setup_medeleg() {
+    use riscv::register::medeleg::*;
+    unsafe {
+        set_breakpoint(); // Breakpoint Delegate
+        set_illegal_instruction(); //Illegal Instruction Delegate
+        set_instruction_fault(); //Instruction Access Fault Delegate
+        set_instruction_misaligned(); //Instruction Address Misaligned Delegate
+        set_instruction_page_fault(); //Instruction Page Fault Delegate
+        set_load_fault(); //Load Access Fault Delegate
+        set_load_misaligned(); //Load Address Misaligned Delegate
+        set_load_page_fault(); //Load Page Fault Delegate
+        set_machine_env_call(); //Environment Call from M-mode Delegate
+        set_store_fault(); //Store/AMO Access fault
+        set_store_misaligned(); //Store/AMO Address Misaligned Delegate
+        set_store_page_fault(); //Store/AMO Page Fault Delegate
+        set_supervisor_env_call(); //Environment Call from S-mode Delegate
+        set_user_env_call(); //Environment Call from U-mode Delegate
+    }
+}
+
+pub(crate) fn setup_mideleg() {
+    use riscv::register::mideleg::*;
+    unsafe {
+        set_sext(); // Supervisor External Interrupt Delegate
+        set_ssoft(); //Supervisor Software Interrupt Delegate
+        set_stimer(); //Supervisor Timer Interrupt Delegate
+        set_uext(); //User External Interrupt Delegate
+        set_usoft(); //User Software Interrupt Delegate
+        set_utimer(); //User Timer Interrupt Delegate
+    }
+}
+
+pub(crate) fn setup_sie() {
+    use riscv::register;
+    unsafe {
+        register::sie::set_sext();
+        register::sie::set_ssoft();
+        register::sie::set_stimer();
+    }
+}
--- src/start.rs
+++ src/start.rs
@@ -1,26 +1,39 @@
 //=================================
 // jump from entry.S
 
+use crate::kmain;
+use crate::risc;
 use core::arch::asm;
 #[allow(dead_code)]
 #[no_mangle]
 extern "C" fn start() {
     use riscv::register;
 
+    // set M Previous Privilege mode to Supervisor, for mret.
     unsafe {
-        // set M Previous Privilege mode to Supervisor, for mret.
         register::mstatus::set_mpp(register::mstatus::MPP::Supervisor);
-        // TODO: set M Exception Program Counter to main, for mret.
     }
-    register::satp::write(0); // disable the page table
+    // set M Exception Program Counter to main, for mret.
+    register::mepc::write(kmain as usize);
 
-    // TODO : delegate all interrupts and exceptions to supervisor mode.
+    register::satp::write(0); // disable the page table for now
+
+    // Tdelegate all interrupts and exceptions to supervisor mode.
+    risc::setup_medeleg();
+    risc::setup_mideleg();
+    risc::setup_sie();
 
     // configure Physical Memory Protection to give supervisor mode
     // access to all of physical memory.
+    register::pmpaddr0::write(0x3fffffffffffff);
+    register::pmpcfg0::write(0xf);
+
+    // ask for clock interrupts.
     timer_init();
 
     // keep each CPU's hartid in its tp register, for cpuid().
+    let hart_id = register::mhartid::read();
+    risc::tp::write(hart_id);
 
     // jump to kmain
     unsafe {
